61bf6c5901b0a55bb591ca919d1e53543dff120f
mancuoj <mancuoj@gmail.com>
2024-06-28 00:35:56 +0800

    feat: ch3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { dmSans } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${dmSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,10 @@
+import { DM_Sans, Lusitana } from 'next/font/google';
+
+export const dmSans = DM_Sans({
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
